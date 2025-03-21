aa53746e6088c41979e405d0216d83f4291f2092
islamemon59 <islamemon59@gmail.com>
2025-03-21 12:15:03 +0600

    bmi contion added

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ const calculateBmi = () => {
     // const bmi = document.getElementById("bmi").value;
     const convertedWeight = parseInt(weight)
     const convertedHeight = parseInt(height)
+
+    if(weight < 0 || weight === "" || height < 0 || height === ""){
+        alert("Input a positive valid number");
+        return;
+    }
+
     console.log(convertedHeight)
     console.log(convertedWeight)
     const convertedToMeter = (convertedHeight / 100) * (convertedHeight / 100);
     console.log(convertedToMeter)
 
     const bmi = (convertedWeight / convertedToMeter).toFixed(2)
+
+
+    if(bmi < 18.5){
+        alert("You are Underweight");
+    }
+    else if(bmi > 18.5 && bmi < 24.9){
+        alert("You are Healthy");
+    }
+    else if(bmi > 25.0 && bmi < 29.9){
+        alert("You are Overweight");
+    }
+    else {
+        alert("You are Obese")
+    }
 
     document.getElementById("bmi").value = bmi;
 }
